8b2816c8a10609f13a1e20a2ae0ba75132260153
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-12 01:06:39 +0530

    Initial commit
    build.sh, shell script to --/build/-- whole "LogoType®" in one go.
    Usage: $ sh build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+# ============================ LogoType® ============================
+#                             ~~ CBBL® ~~
+#           Computational Biology and Bioinformatics Laboratory
+#         Υπολογιστικής Βιολογίας και Βιοπληροφορικής Εργαστήριο
+#         Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+# Copyright © 2014 - 2016, Sequømics Corporation. All rights reserved.
+# ====================================================================
