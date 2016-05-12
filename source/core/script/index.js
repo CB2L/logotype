b30f61b11da7fbba41afccf5d05f291669a6b43a
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-12 16:00:30 +0530

    Initial commit
    LogoType® of CBBL® - Computational Biology and Bioinformatics Laboratory.

---
+++ source/core/script/index.js
@@ -0,0 +1,21 @@
+/*!
+ * LogoType®
+ * ~~~~~~~~~
+ * CBBL®: Computational Biology and Bioinformatics Laboratory
+ *        Υπολογιστικής Βιολογίας και Βιοπληροφορικής Εργαστήριο
+ * ______________________________________________________________
+ * Grunt, http://gruntjs.com/.
+ * The JavaScript Task Runner.
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ______________________________________________________________
+ * @date      : 11-May-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * ______________________________________________________________
+ */
